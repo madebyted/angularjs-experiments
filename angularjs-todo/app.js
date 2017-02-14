d9466fc5fe2bba1e9de0650d2f9c73328233a2de
madebyted <ted.dominiak@gmail.com>
2017-02-14 22:52:50 +0200

    added angularjs todo list app

---
+++ angularjs-todo/app.js
@@ -0,0 +1,37 @@
+var app = angular.module('simple-todo', ['ngCookies']);
+
+app.controller('todoList', function($scope, $cookies, $cookieStore) {
+    var todoTasks = $cookieStore.get('todo'),
+        doneTasks = $cookieStore.get('done');
+
+    $scope.tasks = {
+        todo: todoTasks ? todoTasks.split('|') : [],
+        done: doneTasks ? doneTasks.split('|') : []
+    };
+
+    $scope.addTask = function(key = null) {
+        if($scope.taskText && (!key || key.which === 13)) {
+            $scope.tasks.todo.push($scope.taskText);
+            $scope.taskText = '';
+
+            $cookieStore.put('todo', $scope.tasks.todo.join('|'));
+        }
+    }
+
+    $scope.updateStatus = function(key) {
+        if(this.todo) {
+            $scope.tasks.done.unshift(
+                $scope.tasks.todo.splice(key, 1)[0]
+            );
+        }
+
+        else if(this.done) {
+            $scope.tasks.todo.unshift(
+                $scope.tasks.done.splice(key, 1)[0]
+            );
+        }
+
+        $cookieStore.put('done', $scope.tasks.done.join('|'));
+        $cookieStore.put('todo', $scope.tasks.todo.join('|'));
+    }
+});
